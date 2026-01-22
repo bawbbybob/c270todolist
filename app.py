8cf2d59e1bb224e79a78e792082e88db9b58d6a9
kh4irvlhad1f93 <24000482@myrp.edu.sg>
2026-01-22 15:58:53 +0800

    app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from calculator import Calculator
+
+app = Flask(__name__)
+calc = Calculator()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = ""
+    if request.method == 'POST':
+        num1 = float(request.form.get('num1'))
+        num2 = float(request.form.get('num2'))
+        operation = request.form.get('operation')
+        
+        if operation == 'add':
+            result = calc.add(num1, num2)
+    return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
